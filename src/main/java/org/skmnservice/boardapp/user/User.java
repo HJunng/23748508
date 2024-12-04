a763567f2769ea3e3dbc2f00c5198723d13d2ead
HJunng <guswjd1gh@gmail.com>
2024-12-04 10:17:44 +0900

    feature : 세션 기반 로그인 구현

--- src/main/java/org/skmnservice/boardapp/user/User.java
+++ src/main/java/org/skmnservice/boardapp/user/User.java
@@ -1,7 +1,7 @@
 package org.skmnservice.boardapp.user;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,11 +10,11 @@ import org.hibernate.annotations.UpdateTimestamp;
 import org.skmnservice.boardapp.board.Post;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
@@ -22,14 +22,20 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id", nullable = false, length = 20, unique = true)
-    private String userId;
+    @Column(name = "username", nullable = false, length = 20, unique = true)
+    private String username;
 
-    @Column(name = "password", nullable = false, length = 50)
+    @Column(name = "password", nullable = false)
     private String password;
 
     @Column(name = "name", length = 50)
     private String name;
+
+    @Column(nullable = false)
+    private String role = "USER"; // 기본 ROLE_USER
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
 
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
@@ -39,6 +45,11 @@ public class User {
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Post> posts;
+    @Builder
+    public User(String username, String password, String name, String role) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.role = (role != null) ? role : "USER"; // null일 경우 기본값 설정
+    }
 }
